59005dec4cc936301810ff2ccdbad79050b29ae3
sh970901 <aaddss639@naver.com>
2024-06-25 00:18:04 +0900

    feat: 비밀번호 수정 로직 추가

--- src/main/java/com/hun/market/member/dto/MemberDto.java
+++ src/main/java/com/hun/market/member/dto/MemberDto.java
@@ -3,9 +3,7 @@ package com.hun.market.member.dto;
 import com.hun.market.backoffice.dto.EmployeeExcelUploadDto;
 import com.hun.market.member.domain.Department;
 import com.hun.market.member.domain.Member;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 public class MemberDto {
@@ -66,13 +64,14 @@ public class MemberDto {
 //        @Size(max = 100, message = "member name must be less than 100 characters")
 //        private String mbName;
 
-        @NotNull(message = "password is required")
+        @NotNull(message = "현재 비밀번호는 필수 입력 값입니다.")
         private String currentPassword;
 
-        @NotNull(message = "password is required")
+        @NotBlank(message = "새 비밀번호는 필수 입력 값입니다.")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String newPassword;
 
-        @NotNull(message = "password is required")
+        @NotNull(message = "새 비밀번호는 필수 입력 값입니다.")
         private String confirmPassword;
     }
 
